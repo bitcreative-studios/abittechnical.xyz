4e0cd6c66377a45f573c4fb98e5f958e1977a3fb
George Bockari <george@revisioned.consulting>
2021-10-01 00:33:04 -0400

    refactor(Hero.tsx): use new display font

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import tw, { styled } from 'twin.macro'
 
-const HeroContent = tw.div`w-full h-full grid grid-rows-8 grid-cols-12 container mx-auto`
+const HeroContent = tw.div`w-full h-full grid grid-rows-8 grid-cols-12 container mx-auto font-text`
 const Brand = styled.div`
   writing-mode: vertical-lr;
-  ${tw`font-heading tracking-wide text-primary text-lg`}
+  ${tw`font-medium tracking-wide text-primary text-lg`}
 `
 const TagLine = styled.div`
   writing-mode: vertical-lr;
-  ${tw`col-start-10 col-end-13 row-start-2 row-end-4 font-heading`}
+  ${tw`col-start-10 col-end-13 row-start-2 row-end-4 font-medium`}
 `
 
 const TopicsBanner = styled.div`
@@ -25,7 +25,7 @@ const Hero = () => (
           bitwhys <span tw="text-gray-900"> / tellusstraighter</span>
         </Brand>
       </div>
-      <h1 tw="w-full row-start-2 row-end-7 col-start-4 col-end-12 grid grid-rows-3 grid-cols-8  gap-x-5 font-heading  lg:text-[12rem] font-medium  leading-tight uppercase">
+      <h1 tw="w-full row-start-2 row-end-7 col-start-4 col-end-12 grid grid-rows-3 grid-cols-8  gap-x-5 font-heading  lg:text-[12rem] font-display font-medium  leading-tight uppercase">
         <div tw="col-start-2 col-end-5 space-x-8">
           <span>a</span>
           <span tw="text-primary">bit</span>
@@ -44,7 +44,7 @@ const Hero = () => (
           to solve it
         </p>
       </TagLine>
-      <div tw="row-start-6 pt-6 row-end-auto col-start-2 col-end-6 font-heading ">
+      <div tw="row-start-6 pt-6 row-end-auto col-start-2 col-end-6">
         <div tw="py-4 flex items-center justify-between border-t-2 border-b-2 border-black">
           <span>Release</span>
           <span>0001</span>
